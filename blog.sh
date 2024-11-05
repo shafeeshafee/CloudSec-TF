6408a5165f9eec55db146ff2fe19c5fd31e10e4f
shaf256 <68356865+shafeeshafee@users.noreply.github.com>
2024-11-04 20:11:11 -0500

    Update blog.sh

--- blog.sh
+++ blog.sh
@@ -35,3 +35,18 @@ sudo systemctl enable microblog
 
 # Check the service status
 sudo systemctl status microblog
+
+
+# Old (insecure) blog.sh
+
+# #!/bin/bash
+# # Update and install dependencies
+# sudo apt update -y
+# sudo apt install -y python3 python3-pip python3-venv
+# # Clone the repository and set up the Flask app
+# git clone https://github.com/kura-labs-org/C5-Cybersecurity-WL1.git /home/ubuntu/microblog
+# cd /home/ubuntu/microblog
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install -r requirements.txt
+# flask run --host=0.0.0.0 > flask.log 2>&1 &
